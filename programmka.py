61bb0515629309a2d0b3b7b8b67df078592ec471
Bigryb <nikki01946@gmail.com>
2023-09-13 12:48:30 +0300

    programmka changed 3

--- programmka.py
+++ programmka.py
@@ -2,5 +2,16 @@ H = "П"
 e = "р"
 l = "и"
 lo = "в"
-World = "ет"
+World = "ет Мир"
 print(H+e+l+lo+World)
+print("Какая сейчас погода? Солнечная или дождливая?")
+weather = input()
+if weather == "Cолнечная" or "солнечная":
+    print(":)")
+elif weather == "Дождливая" or "дождливая":
+    print(":(")
+else:
+    print("Тебя спрашивали про погоду, а не про другое")
+    
+    
+    
