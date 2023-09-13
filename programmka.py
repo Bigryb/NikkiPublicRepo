0bc5d9f25bf465733307554a5501521f48ff9d5c
Bigryb <nikki01946@gmail.com>
2023-09-13 12:41:03 +0300

    programka changed_2

--- programmka.py
+++ programmka.py
@@ -1,11 +1,6 @@
-a = 0
-kol = 0
-b = 10
-while True:
-    if a < b:
-        a += 1
-        kol += 1
-        
-    else:
-        print(kol)
-        break
+H = "П"
+e = "р"
+l = "и"
+lo = "в"
+World = "ет"
+print(H+e+l+lo+World)
